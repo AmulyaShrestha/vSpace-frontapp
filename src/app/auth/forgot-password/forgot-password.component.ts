9d45113eda491735428575c0de19e28bff89627b
Amulya Stha <sthaamulya@gmail.com>
2020-07-11 22:37:54 +0545

    configured forgot password reset token within forgot password comonent

--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthenticationService} from '../../@core/authentication.service';
 import {ToastrService} from 'ngx-toastr';
+import {MatDialogRef} from '@angular/material/dialog';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
@@ -9,23 +11,51 @@ import {ToastrService} from 'ngx-toastr';
 })
 export class ForgotPasswordComponent implements OnInit {
   spinner = false;
+  passwordHide = true;
+  tokenSectionIsActive = false;
+
+  forgotPasswordFormGroup: FormGroup;
 
   constructor(private authService: AuthenticationService,
-              private toastr: ToastrService) {
+              private toastr: ToastrService,
+              private formBuilder: FormBuilder,
+              private dialogRef: MatDialogRef<ForgotPasswordComponent>) {
+  }
+
+  get emailValue() {
+    return this.forgotPasswordFormGroup.get('email').value;
   }
 
   ngOnInit(): void {
+    this.forgotPasswordFormGroup = this.formBuilder.group({
+      email: [undefined, Validators.required],
+      passResetToken: [undefined, Validators.required],
+      password: [undefined, Validators.required]
+    });
   }
 
-  requestPassword(email) {
+  requestPassword() {
     this.spinner = true;
-    this.authService.forgotPasswordSendEmail(email).subscribe((res) => {
-      this.toastr.success('Successful!', `An email has been successfully sent to '${email}'`);
-      console.log(res);
+    this.authService.forgotPasswordSendEmail(this.emailValue).subscribe((res) => {
+      this.toastr.success('Successful!', `An email has been successfully sent to '${this.emailValue}'`);
       this.spinner = false;
-      }, error => {
-      this.toastr.success('Failed!', `The email '${email}' appears to be invalid`);
+      this.tokenSectionIsActive = true;
+    }, error => {
+      this.toastr.error('Failed!', `The email '${this.emailValue}' appears to be invalid`);
       console.log(error);
+      this.spinner = false;
+    });
+  }
+
+  verifyToken() {
+    this.spinner = true;
+    this.authService.resetPassword(this.forgotPasswordFormGroup.value).subscribe(res => {
+      this.toastr.success('Successful!', `Password reset successful for '${this.emailValue}'`);
+      this.dialogRef.close();
+    }, error => {
+      this.toastr.error('Failed!', `Failed to reset password for '${this.emailValue}'`);
+      console.log(error);
+      this.spinner = false;
     });
   }
 }
