62b83d7c8bc2b08891f0daea56dffc0f39953f82
Amulya Stha <sthaamulya@gmail.com>
2020-07-23 22:17:11 +0545

    added environment variables like google api key and news api key for env.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
+  GOOGLE_MAP_API_KEY: 'AIzaSyDKERYllGf5BfVR_c_45nqHVkbHoPgXPDA',
+  THE_NEW_YORK_TIMES_API_KEY: 'altw1Ta9WvJ4PAc5QbmqYNWnIeRUar1k',
   production: true
 };
