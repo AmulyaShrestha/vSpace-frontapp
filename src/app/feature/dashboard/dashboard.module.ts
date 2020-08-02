44734592c3055ec0c469f1e803767928f19ed806
Amulya Stha <sthaamulya@gmail.com>
2020-08-02 23:50:54 +0545

    removed card component from dashboard module

--- src/app/feature/dashboard/dashboard.module.ts
+++ src/app/feature/dashboard/dashboard.module.ts
@@ -3,33 +3,32 @@ import {CommonModule} from '@angular/common';
 
 import {DashboardRoutingModule} from './dashboard-routing.module';
 import {DashboardComponent} from './components/dashboard/dashboard.component';
-import {CardComponent} from './components/card/card.component';
 import {ThemeModule} from '../../@theme/theme.module';
 import {GridsterModule} from 'angular-gridster2';
 import {DynamicModule} from 'ng-dynamic-component';
 import {DashboardService} from './services/dashboard.service';
 import {WidgetCommunicationService} from './services/widget-communication.service';
-import {YoutubeComponent} from './components/youtube/youtube.component';
-import {CoronaWorldMapComponent} from './components/corona-world-map/corona-world-map.component';
-import {CoronaWorldTableComponent} from './components/corona-world-table/corona-world-table.component';
-import {CoronaMetricsComponent} from './components/corona-metrics/corona-metrics.component';
-import {CoronaWorldChartComponent} from './components/corona-world-chart/corona-world-chart.component';
-import {CloclAnalogueComponent} from './components/clock-analogue/clocl-analogue.component';
-import {NepaliCalendarComponent} from './components/nepali-calendar/nepali-calendar.component';
-import {WeatherComponent} from './components/weather/weather.component';
+import {YoutubeComponent} from './components/simple-widgets/youtube/youtube.component';
+import {CoronaWorldMapComponent} from './components/simple-widgets/corona-world-map/corona-world-map.component';
+import {CoronaWorldTableComponent} from './components/simple-widgets/corona-world-table/corona-world-table.component';
+import {CoronaMetricsComponent} from './components/simple-widgets/corona-metrics/corona-metrics.component';
+import {CoronaWorldChartComponent} from './components/simple-widgets/corona-world-chart/corona-world-chart.component';
+import {CloclAnalogueComponent} from './components/simple-widgets/clock-analogue/clocl-analogue.component';
+import {NepaliCalendarComponent} from './components/simple-widgets/nepali-calendar/nepali-calendar.component';
+import {WeatherComponent} from './components/simple-widgets/weather/weather.component';
 import {StickyNoteComponent} from './components/sticky-note/sticky-note.component';
 import {CoreModule} from '../../@core/core.module';
-import { AddNotesComponent } from './components/add-notes/add-notes.component';
+import { AddNotesComponent } from './components/dialog-components/add-notes/add-notes.component';
 import { ToDoComponent } from './components/to-do/to-do.component';
 import { NewYorkTimesNewsComponentComponent } from './components/advanced-widgets/new-york-times-news-component/new-york-times-news-component.component';
-import { UpdateProfileModalComponent } from './components/update-profile-modal/update-profile-modal.component';
+import { UpdateProfileModalComponent } from './components/dialog-components/update-profile-modal/update-profile-modal.component';
 import { LocationViewerComponent } from './components/advanced-widgets/location-viewer/location-viewer.component';
 import { AddLocationModalComponent } from './components/advanced-widgets/add-location-modal/add-location-modal.component';
+import { WidgetSettingsDialogComponent } from './components/dialog-components/widget-settings-dialog/widget-settings-dialog.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    CardComponent,
     YoutubeComponent,
     CoronaWorldMapComponent,
     CoronaWorldTableComponent,
@@ -44,7 +43,8 @@ import { AddLocationModalComponent } from './components/advanced-widgets/add-loc
     NewYorkTimesNewsComponentComponent,
     UpdateProfileModalComponent,
     LocationViewerComponent,
-    AddLocationModalComponent],
+    AddLocationModalComponent,
+    WidgetSettingsDialogComponent],
   imports: [
     CommonModule,
     ThemeModule,
